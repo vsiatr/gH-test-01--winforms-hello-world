291d4cced232fb6b8cf716b12c6148fd7f970924
Vassilis Siatravanis <vsiatr@hotmail.com>
2016-05-17 15:33:31 +0300

    added code to button2

--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,8 @@
    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
       MessageBox.Show("hello world")
    End Sub
+
+   Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+      MessageBox.Show("hello world 2")
+   End Sub
 End Class
